4f234b2800be8b83e178438a3cabb426652a88d8
Taro Minowa <higepon@gmail.com>
2018-02-07 18:07:19 +0900

    Adds filter.

--- textify.py
+++ textify.py
@@ -1,11 +1,13 @@
 import argparse
 import sqlite3
 import sys
+import re
 
 
 class Dumper:
     def __init__(self, db):
         self.db = db
+        self.url_regexp = re.compile(r"https?:\/\/")
 
     def dump(self):
         conn = sqlite3.connect(self.db)
@@ -18,10 +20,18 @@ class Dumper:
             text1 = self.tweet_text(conn, sid1)
             text2 = self.tweet_text(conn, sid2)
             text3 = self.tweet_text(conn, sid3)
-            if text1 is None or text2 is None or text3 is None:
-                continue
-            print("{}\n{}\n{}".format(text1, text2, text3))
+            if self.is_valid_conversation(text1, text2, text3):
+                print("{}\n{}\n{}".format(text1, text2, text3))
         conn.close()
+
+    def is_valid_conversation(self, text1, text2, text3):
+        if text1 is None or text2 is None or text3 is None:
+            return False
+        for text in [text1, text2, text3]:
+            if re.search(self.url_regexp, text):
+                return False
+
+        return True
 
     @staticmethod
     def tweet_text(conn, sid):
